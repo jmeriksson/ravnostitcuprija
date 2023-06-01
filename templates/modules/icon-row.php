7fb4228e6485c8d28580412fe45355e45c54303d
Mikael Eriksson <mikael.eriksson.nkpg@gmail.com>
2023-06-01 09:36:53 +0200

    adding new icons

--- templates/modules/icon-row.php
+++ templates/modules/icon-row.php
@@ -6,6 +6,7 @@
  */
 
 $icons = $args['icons'] ?? [];
+$icon_style = $args['icon_style'] ?? '';
 
 ?>
 
@@ -17,7 +18,19 @@ $icons = $args['icons'] ?? [];
                     <?php foreach ($icons as $icon) : ?>
                         <div class="icon">
                             <div class="image">
-                                <?php echo file_get_contents( get_template_directory() . "/assets/images/icons/" . $icon['icon'] . ".svg") ?>
+                                <?php
+                                if (isset($icon['link']) && !empty($icon['link'])) {
+                                    printf(
+                                        '<a href="%s" target="%s">',
+                                        esc_url($icon['link']['url']),
+                                        esc_attr($icon['link']['target'])
+                                    );
+                                }
+                                echo file_get_contents( get_template_directory() . "/assets/images/icons/" . $icon['icon'] . '-' . $icon_style . ".svg");
+                                if (isset($icon['link']) && !empty($icon['link'])) {
+                                    echo '</a>';
+                                }
+                                ?>
                             </div>
                             <?php if ($icon['text_content']) : ?>
                                 <div class="content">
